3ed178f3d5bc5618bcd71fe206aeef86a97f8dc0
typar <typarris421@gmail.com>
2021-09-05 00:56:37 -0400

    added main app and flask lib

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres123@localhost/height_collector'
+db = SQLAlchemy(app)
+
+# bluepoint to interact with sqlalchemy
+
+
+class Data(db.Model):
+    __tablename__ = "data"
+    id = db.Column(db.Integer, primary_key=True)
+    email_ = db.Column(db.String(120), unique=True)
+    height_ = db.Column(db.Integer)
+
+    def __init__(self, email_, height_):
+        self.email_ = email_
+        self.height_ = height_
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/success", methods=['POST'])
+def success():
+    if request.method == 'POST':
+        email = request.form["email_name"]
+        height = request.form["height_name"]
+        print(email, height)
+        # email field is equal to email variable
+        if db.session.query(Data).filter(Data.email_ == email):
+            data = Data(email, height)
+            db.session.add(data)
+            db.session.commit()
+            return render_template("success.html")
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
